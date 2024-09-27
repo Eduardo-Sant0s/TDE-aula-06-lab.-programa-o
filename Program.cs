7213390760002b028b5220e19961da942447b681
Eduardo dos Santos Selau <102335143+Eduardo-Sant0s@users.noreply.github.com>
2024-09-26 21:43:50 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -12,10 +12,10 @@ Console.WriteLine($"A área do retangulo R3 é = {R3.CalcularArea()}");
 Console.WriteLine($"O perímetro do retangulo R3 é = {R3.CalcularPerimetro()}");
 
 R3.Redimensionar(2);
-Console.WriteLine($"A área do retangulo R2 é = {R3.CalcularArea()}");
-Console.WriteLine($"O perímetro do retangulo R2 é = {R3.CalcularPerimetro()}");
+Console.WriteLine($"A área do retangulo R3 é = {R3.CalcularArea()}");
+Console.WriteLine($"O perímetro do retangulo R3 é = {R3.CalcularPerimetro()}");
 
 R2.Redimensionar(10,15);
-Console.WriteLine($"A área do retangulo R3 é = {R2.CalcularArea()}");
-Console.WriteLine($"O perímetro do retangulo R3 é = {R2.CalcularPerimetro()}");
+Console.WriteLine($"A área do retangulo R2 é = {R2.CalcularArea()}");
+Console.WriteLine($"O perímetro do retangulo R2 é = {R2.CalcularPerimetro()}");
 
